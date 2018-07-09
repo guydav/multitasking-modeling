008e8733655260dc01aad588e3449569396bb262
Guy Davidson <guydav@gmail.com>
2018-07-09 18:46:22 -0400

    Got the multitasking model to a point it look like it's training, slowly but surely.

--- train_multitasking_model.py
+++ train_multitasking_model.py
@@ -1,10 +1,12 @@
 import numpy as np
 import psyneulink as pnl
 import multitasking_nn_model
+from pattern_generation import generate_training_patterns
 
 # Setting up defaults
 DEFAULT_TRAIN_ITERATIONS = 500
 DEFAULT_NUM_REPLICATIONS = 100
+DEFUALT_TASK_IDS = [0, 4, 5, 8]
 
 DEFAULT_NUM_DIMENSIONS = 3
 DEFAULT_NUM_FEATURES_PER_DIMENSION = 4
@@ -18,7 +20,18 @@ def load_or_create_patterns(num_dimensions=DEFAULT_NUM_DIMENSIONS,
 
 def main():
     model = multitasking_nn_model.MultitaskingModel(DEFAULT_NUM_DIMENSIONS, DEFAULT_NUM_FEATURES_PER_DIMENSION)
-    model.system.show_graph(show_dimensions=pnl.ALL, show_projection_labels=pnl.ALL, show_processes=pnl.ALL) #show_processes=pnl.ALL)
+    # model.system.show_graph(show_dimensions=pnl.ALL, show_projection_labels=pnl.ALL, show_processes=pnl.ALL)
+
+    # print(model.train([[[1, 0, 0, 0]], [[0, 1, 0, 0]], [[0, 0, 1, 0]]],
+    #           [[1, 0, 0, 0, 0, 0, 0, 0, 0]],
+    #           [[[1, 0, 0, 0]], [[0, 0, 0, 0]], [[0, 0, 0, 0]]]))
+
+    input_patterns, task_patterns, in_out_map, target_patterns = generate_training_patterns(DEFAULT_NUM_DIMENSIONS,
+                                                                                            DEFAULT_NUM_FEATURES_PER_DIMENSION)
+    task_indices = np.argmax(task_patterns, 1)
+    trial_indices = np.isin(task_indices, DEFUALT_TASK_IDS)
+
+    print(model.train(input_patterns[trial_indices], task_patterns[trial_indices], target_patterns[trial_indices], 10))
 
 
 if __name__ == '__main__':
